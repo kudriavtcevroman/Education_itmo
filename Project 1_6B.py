3bfd5ab8f46d4233f5d3b4792c5a7f6b634bbff3
Роман <kudriavtcevroman@mail.ru>
2024-06-20 15:21:53 +0300

    First Commit

--- Project 1_6B.py
+++ Project 1_6B.py
@@ -3,14 +3,11 @@ from pathlib import Path
 from datetime import datetime
 
 work = Path.cwd()
-print(work)
 for file in work.glob('*.csv'):
     rows = []
     with open(file, "r", encoding="utf-8") as fh:
         reader = csv.reader(fh)
         rows = list(reader)
-        print(rows)
-        print(file.stem)
     time = datetime.now().strftime('%H_%M_%S_%d_%m_%Y')
     file_copy = f'{file.stem}_{time}.txt'
     with open(file_copy, 'w', encoding="utf-8", newline='') as fl:
@@ -18,3 +15,4 @@ for file in work.glob('*.csv'):
             fl.write(' '.join(row) + '\n')
     with open('log_file.txt', 'a', encoding="utf-8", newline='') as log_file:
         log_file.write(f'Файл {file.stem} скопирован в {file_copy}. "{time}"' + '\n')
+print('Файлы скопированы')
