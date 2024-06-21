8904505c28500c7c06260ef9fdf1a64daffc0cfc
Роман <kudriavtcevroman@mail.ru>
2024-06-21 09:32:20 +0300

    First Commit

--- Project 1_6B.py
+++ Project 1_6B.py
@@ -1,18 +1,28 @@
 import csv
-from pathlib import Path
-from datetime import datetime
+import datetime
+import logging
 
-work = Path.cwd()
-for file in work.glob('*.csv'):
-    rows = []
-    with open(file, "r", encoding="utf-8") as fh:
-        reader = csv.reader(fh)
-        rows = list(reader)
-    time = datetime.now().strftime('%H_%M_%S_%d_%m_%Y')
-    file_copy = f'{file.stem}_{time}.txt'
-    with open(file_copy, 'w', encoding="utf-8", newline='') as fl:
-        for row in rows:
-            fl.write(' '.join(row) + '\n')
-    with open('log_file.txt', 'a', encoding="utf-8", newline='') as log_file:
-        log_file.write(f'Файл {file.stem} скопирован в {file_copy}. "{time}"' + '\n')
-print('Файлы скопированы')
+timestring = datetime.datetime.utcnow().strftime("%Y%m%d_%H_%M_%S")
+logfile = 'log_file_' + timestring + '.log'
+files_csv = ['gamers_dict_lucky_cube.csv', 'orderdata_sample.csv', 'wrong_file_3.csv', 'orderdata_sample_new.csv', 'wrong_file_1.csv', 'wrong_file_2.csv']
+
+logging.basicConfig(filename=logfile, level=logging.DEBUG)
+
+def read_file(file):
+    today = datetime.datetime.today().strftime("%Y-%m-%d %H.%M.%S")
+    try:
+        with open(file, "r", encoding="utf-8") as fl:
+            reader = csv.reader(fl)
+            rows = list(reader)
+        file_copy = f'{file}_{today}.txt'
+        with open(file_copy, 'w', encoding="utf-8", newline='') as fc:
+            for row in rows:
+                fc.write(' '.join(row) + '\n')
+        with open(logfile, 'a', encoding="utf-8", newline='') as log:
+            log.write(f'Файл {file} скопирован в {file_copy}. "{today}"' + '\n')
+    except FileNotFoundError as err:
+        print(f'Файла {file}, не существует')
+        logging.error(today + ": " + str(err))
+
+for file in files_csv:
+    read_file(file)
