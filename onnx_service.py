1bd68546eee5512de00eb9491312f8b72d111c43
Roman <119002593+kudriavtcevroman@users.noreply.github.com>
2024-11-21 12:37:06 +0300

    Update onnx_service.py

--- onnx_service.py
+++ onnx_service.py
@@ -1,38 +1,27 @@
 from __future__ import annotations
 import bentoml
-import onnxruntime as ort
-import numpy as np
-from PIL import Image
-from bentoml.io import JSON
+from transformers import pipeline
 
-# Создаём BentoML сервис с использованием декоратора
-@bentoml.service()
-class ONNXModelService:
-    def __init__(self):
-        # Загрузка ONNX модели
-        self.session = ort.InferenceSession("/content/yolov8n_SGD.onnx")
-        self.input_name = self.session.get_inputs()[0].name
-        self.output_name = self.session.get_outputs()[0].name
 
-    @bentoml.api(input=JSON(), output=JSON())
-    async def predict(self, input_sample: dict) -> dict:
-        """
-        Выполнение инференса модели.
-        Args:
-            input_sample (dict): JSON с путём к изображению.
-        Returns:
-            dict: Результаты предсказания модели.
-        """
-        # Предобработка изображения
-        image_path = input_sample["image_path"]
-        image = Image.open(image_path).convert("RGB")
-        image_array = np.asarray(image).astype(np.float32)
-        image_array = np.transpose(image_array, (2, 0, 1))  # Преобразование в CHW
-        image_array = np.expand_dims(image_array, axis=0) / 255.0  # Нормализация
+EXAMPLE_INPUT = "sometext"
 
-        # Выполнение инференса
-        predictions = self.session.run([self.output_name], {self.input_name: image_array})
+@bentoml.service(
+    resources={"cpu": "2"},
+    traffic={"timeout": 10},
+)
 
-        # Постобработка результатов
-        result = predictions[0].tolist()
-        return {"predictions": result}
+class Model:
+    def __init__(self) -> None:
+        # Load onnx model and start onn inference service
+        self.pipeline = pipeline('summarization')
+
+
+    @bentoml.api
+    def predict(self, input_sample: = EXAMPLE_INPUT) -> str:
+  # dont forget to add preprocessing  module for input data
+
+        result = self.pipeline(input_sample)
+
+  # dont forget to add post processing  module for input data
+
+        return result[0]['summary_text'] #or extract target information
