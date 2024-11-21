0726714fb9d8ef8146d301797fa59b498b35c9ad
Roman <119002593+kudriavtcevroman@users.noreply.github.com>
2024-11-21 12:51:59 +0300

    Update onnx_service.py

--- onnx_service.py
+++ onnx_service.py
@@ -1,27 +1,41 @@
 from __future__ import annotations
 import bentoml
-from transformers import pipeline
+import onnxruntime as ort
+import numpy as np
+from PIL import Image
 
-
-EXAMPLE_INPUT = "sometext"
+EXAMPLE_INPUT = {"image_path": "/content/Datasets/Bee_or_Wasp_Subset/validation/bees/117423537_2f4372aa4e_m.jpg"}
 
 @bentoml.service(
     resources={"cpu": "2"},
     traffic={"timeout": 10},
 )
-
 class Model:
     def __init__(self) -> None:
-        # Load onnx model and start onn inference service
-        self.pipeline = pipeline('summarization')
-
+        # Загружаем ONNX модель
+        self.session = ort.InferenceSession("/content/yolov8n_SGD.onnx")
+        self.input_name = self.session.get_inputs()[0].name
+        self.output_name = self.session.get_outputs()[0].name
 
     @bentoml.api
-    def predict(self, input_sample: = EXAMPLE_INPUT) -> str:
-  # dont forget to add preprocessing  module for input data
+    def predict(self, input_sample: dict = EXAMPLE_INPUT) -> dict:
+        """
+        Выполнение инференса модели.
+        Args:
+            input_sample (dict): JSON с путём к изображению.
+        Returns:
+            dict: Результаты предсказания модели.
+        """
+        # Предобработка данных
+        image_path = input_sample["image_path"]
+        image = Image.open(image_path).convert("RGB")
+        image_array = np.asarray(image).astype(np.float32)
+        image_array = np.transpose(image_array, (2, 0, 1))  # Преобразование в CHW
+        image_array = np.expand_dims(image_array, axis=0) / 255.0  # Нормализация
 
-        result = self.pipeline(input_sample)
+        # Выполнение инференса
+        predictions = self.session.run([self.output_name], {self.input_name: image_array})
 
-  # dont forget to add post processing  module for input data
-
-        return result[0]['summary_text'] #or extract target information
+        # Постобработка
+        result = predictions[0].tolist()  # Преобразование в список
+        return {"predictions": result}
